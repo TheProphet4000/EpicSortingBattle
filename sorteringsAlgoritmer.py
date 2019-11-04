088de4deb2b65c68db3210a4f85c77cf25295121
Mikkel Andreasen <andreasenmikkel1@gmail.com>
2019-11-04 14:32:11 +0100

    Print

--- sorteringsAlgoritmer.py
+++ sorteringsAlgoritmer.py
@@ -1,10 +1,9 @@
 import sys
 
 #her laver vi en en variabel kladt j, der læser MiniTest.txt filen, og viser dens indhold
-j = open("MiniTest.txt", "r")
-print(j.read())
+j = open("MiniTest.txt")
 
 #blader alt indhold igennem, i variablen j
 for i in range(len(j)):
-
+    print(i)
 #selectionSort()
